82cd25bc2160c6689c971133a9bc5c258111e204
Paolo Cittadini <cittadini.pa@gmail.com>
2016-12-18 09:51:03 +0100

    pycity-0 : actions.py, renamed map to world
    Addedd readme file

--- actions.py
+++ actions.py
@@ -6,45 +6,46 @@ BUILDING_TYPE_FARM = "FARM"
 BUILDING_TYPE_FACTORY = "FACT"
 
 
-def build_street(map, i, j):
+def build_street(world, i, j):
 
-    if map.get_terrain_unit(i, j) == 0:
-        map.set_terrain_unit(i, j, BUILDING_TYPE_STREET)
+    if world.get_terrain_unit(i, j) == 0:
+        world.set_terrain_unit(i, j, BUILDING_TYPE_STREET)
         return True
 
     return False
 
 
-def build_house(map, i, j):
+def build_house(world, i, j):
 
-    if map.get_terrain_unit(i, j) == 0 and is_close_to(map, i , j, BUILDING_TYPE_STREET):
-        map.set_terrain_unit(i, j, BUILDING_TYPE_HOUSE)
+    if world.get_terrain_unit(i, j) == 0 and is_close_to(world, i, j, BUILDING_TYPE_STREET):
+        world.set_terrain_unit(i, j, BUILDING_TYPE_HOUSE)
         return True
 
     return False
 
 
-def build_factory(map, i, j):
+def build_factory(world, i, j):
 
-    if map.get_terrain_unit(i, j) == 0 and is_close_to(map, i, j, terrain.RESOURCE_ORE) and is_close_to(map, i, j, BUILDING_TYPE_STREET):
-        map.set_terrain_unit(i, j, BUILDING_TYPE_FACTORY)
-        return True
-
-    return False
-
-def build_farm(map, i, j):
-
-    if map.get_terrain_unit(i, j) == terrain.PERCENTAGE_FOOD and is_close_to(map, i, j, BUILDING_TYPE_STREET):
-        map.set_terrain_unit(i, j, BUILDING_TYPE_FARM)
+    if world.get_terrain_unit(i, j) == 0 and is_close_to(world, i, j, terrain.RESOURCE_ORE) and is_close_to(world, i, j, BUILDING_TYPE_STREET):
+        world.set_terrain_unit(i, j, BUILDING_TYPE_FACTORY)
         return True
 
     return False
 
 
-def is_close_to(map, i, j, element):
+def build_farm(world, i, j):
+
+    if world.get_terrain_unit(i, j) == terrain.PERCENTAGE_FOOD and is_close_to(world, i, j, BUILDING_TYPE_STREET):
+        world.set_terrain_unit(i, j, BUILDING_TYPE_FARM)
+        return True
+
+    return False
+
+
+def is_close_to(world, i, j, element):
 
     for unit in terrain.get_close_indexes(i, j):
-        if map.get_terrain_unit(unit[0], unit[1]) == element:
+        if world.get_terrain_unit(unit[0], unit[1]) == element:
             return True
 
     return False
